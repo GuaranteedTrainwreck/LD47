46afdf32b2988ecc937249c2d682c61297a6a7fe
damiengurzynski <damiengurzynski@hotmail.fr>
2020-10-04 21:23:38 +0200

    stuff

--- Scripts/Staffcabin/Staffcabin.gd
+++ Scripts/Staffcabin/Staffcabin.gd
@@ -7,24 +7,31 @@ var beingCool = false
 func _ready():
 	pass
 
-func _physics_process(delta):
-	if selected:
-		self.rotation_degrees = 180
-	else:
-		self.rotation_degrees = 0
+#func _physics_process(delta):
+#	if selected:
+#		self.rotation_degrees = 180
+#	else:
+#		self.rotation_degrees = 0
 
 func _on_Staffcabin_input_event(viewport, event, shape_idx):
 	if event.is_action_pressed("player_select") and !beingCool:
 		selected = true
-		get_parent().get_node("HideScreen").visible = true
 		$CabinTimer.set_wait_time(1)
 		$CabinTimer.start()
 		beingCool = true
+		$AnimationPlayer.play("selected")
+		get_parent().get_node("Clouds").visible = true
+		get_parent().get_node("talkinghead").visible = true
+		get_parent().get_node("BeingCool").play("smoke")
 	elif event.is_action_pressed("player_select") and beingCool:
-		get_parent().get_node("HideScreen").visible = false
 		selected = false
 		beingCool = false
 		$CabinTimer.stop()
+		$AnimationPlayer.stop()
+		$AnimationPlayer.seek(0, true)
+		get_parent().get_node("Clouds").visible = false
+		get_parent().get_node("talkinghead").visible = false
+		get_parent().get_node("BeingCool").stop()
 
 func _on_CabinTimer_timeout():
 	get_parent().coolness += 2
--- Scripts/World/World.gd
+++ Scripts/World/World.gd
@@ -7,7 +7,12 @@ const HOOL = preload("res://Scenes/Hoopers/HulaHooper.tscn")
 var deathsTotal = 0
 var incidentsTotal = 0
 var coolness = 50
+var chaos = 0
+var coolnessSubFactor = 1
 export var spawnRate = 3
+
+#booleans
+var onPhone = false
 
 #get timer
 onready var timer = get_node("GlobalTimer")
@@ -20,10 +25,12 @@ func _ready():
 
 #AT RUNTIME
 func _physics_process(delta):
+	chaos = deathsTotal + incidentsTotal
 #	UI changes
 	get_node("Deaths").text = "Deaths : " + str(deathsTotal)
 	get_node("Incidents").text = "Incidents : " + str(incidentsTotal)
 	get_node("Coolness").text = "Coolness : " + str(coolness)
+	get_node("Chaos").text = "Chaos : " + str(chaos)
 	
 #	spawn Hoopers
 	if !spawnRate:
@@ -36,5 +43,13 @@ func _on_GlobalTimer_timeout():
 	spawnRate -= 1
 	
 	if get_node("Staffcabin").beingCool == false:
-		coolness -= 1
-
+		coolness -= coolnessSubFactor
+	
+#	boss calls
+	if chaos > 5:
+		onPhone = true
+		$talkinghead.visible = true
+		$talkinghead/talktest.text = "HEY ! VINCE TOLD ME THERE WAS SOME TROUBLE\nAT THE CLUB...\nEVERYTHING OK ?"
+		coolnessSubFactor = 2
+	
+	
